69c7ab8ff065c93e223ffc140faf55f56c53c0d6
ianlp <casualhouse@yahoo.com>
2024-07-09 17:38:05 -0500

    main files

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'www.ianfromchicago.com'
+    site: 'https://your-username.github.io'
 });
