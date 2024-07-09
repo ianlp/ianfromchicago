c2bd9ee2fffff1e596636e50b1f82ffd5695072c
ianlp <casualhouse@yahoo.com>
2024-07-09 18:58:15 -0500

    undo

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'http://www.ianfromchicago.com/',
-  // ... other configuration options ...
-});
